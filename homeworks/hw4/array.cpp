cbf2e61fa54c6db1815bef1e7d7f45699e76ce79
Yizhen Jia <stephenjia3852@gmail.com>
2022-11-19 23:20:51 -0500

    update on array.cpp

--- homeworks/hw4/array.cpp
+++ homeworks/hw4/array.cpp
@@ -1,8 +1,39 @@
 #include "array.hpp"
 #include <iostream>
 
-Array::Array(){}
+Array::Array(){
+    this->_size = 0;
+    this->_data = NULL;
+}
 
-void Array::
+Array::Array(const Array &other){
+    this->_data = new double[this->_size];
+    this->_size = other._size;
+    for (unsigned int i = 0; i < this->_size ; i++){
+    this->_data[i] = other._data[i];
+    }
+}
+
+Array::Array(unsigned n, double v){
+    this->_size = n;
+    this->_data = new double[this->_size];
+    for (unsigned int i = 0;i < this->_size ; i++){
+    this->_data[i] = v;
+    }
+}
+
+void Array::copy(const Array &rhs){}
+
+void Array::resize(unsigned new_size, bool prsv){}
+
+double Array::norm() const{}
+
+double Array::sum() const{}
+
+double Array::max() const{}
+
+double Array::min() const{}
+
+double Array::dot(const Array &rhs) const{}
 
 void Array::print() const{}
